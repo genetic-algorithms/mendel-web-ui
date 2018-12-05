2e0f6cc7455f3e29c87536c82cbde058dbd71e8d
Jonathan Potter <jpotterm@gmail.com>
2018-12-05 10:47:21 -0500

    Put react in production mode

--- cmd/server/base_template.go
+++ cmd/server/base_template.go
@@ -16,8 +16,8 @@ var baseTemplate = `
 		<body>
 			<div id="react-root"></div>
 
-			<script src="https://unpkg.com/react@16.4.2/umd/react.development.js"></script>
-			<script src="https://unpkg.com/react-dom@16.4.2/umd/react-dom.development.js"></script>
+			<script src="https://unpkg.com/react@16.4.2/umd/react.production.min.js"></script>
+			<script src="https://unpkg.com/react-dom@16.4.2/umd/react-dom.production.min.js"></script>
 			<script src="https://unpkg.com/redux@4.0.0/dist/redux.min.js"></script>
 			<script src="https://unpkg.com/react-redux@5.0.7/dist/react-redux.min.js"></script>
 			<script src="https://unpkg.com/immer@1.5.0/dist/immer.umd.js"></script>
